0429d551418bdf8ed74871fb38c0e45c2685d3ab
erikandersson96 <erik96.andersson@gmail.com>
2022-02-01 12:11:07 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+function rungame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
